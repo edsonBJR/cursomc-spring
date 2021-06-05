7d832b7a04fe4b54d73e322221bc12ee1e59e97e
Edson Junior <ebjr.tux@gmail.com>
2021-06-05 11:24:42 -0300

    Endpoint /pedidos/{id} disponibilizado

---
+++ src/main/java/com/edsonbjr/cursomc/services/PedidoService.java
@@ -0,0 +1,25 @@
+package com.edsonbjr.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.edsonbjr.cursomc.domain.Pedido;
+import com.edsonbjr.cursomc.repositories.PedidoRepository;
+import com.edsonbjr.cursomc.services.exceptions.ObjectNotFoundException;
+
+
+@Service
+public class PedidoService {
+	
+	@Autowired
+	private PedidoRepository repo;
+	
+	public Pedido buscar(Integer id) {
+		Optional<Pedido> categoria = repo.findById(id);
+		return categoria.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
+	}
+
+}
