207cf07947e9c450fc86e8fa029af9265ac183bf
Edson Junior <ebjr.tux@gmail.com>
2021-06-05 09:35:20 -0300

    Pedido, EstadoPagamento e Pagamento

--- src/main/java/com/edsonbjr/cursomc/domain/PagamentoComCartao.java
+++ src/main/java/com/edsonbjr/cursomc/domain/PagamentoComCartao.java
@@ -1,7 +1,10 @@
 package com.edsonbjr.cursomc.domain;
 
+import javax.persistence.Entity;
+
 import com.edsonbjr.cursomc.domain.enums.EstadoPagamento;
 
+@Entity
 public class PagamentoComCartao extends Pagamento  {
 	private static final long serialVersionUID = 1L;
 	
