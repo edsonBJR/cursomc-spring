3cd38f3be4ea2a2c4ab9a73680c668537742bc85
Edson Junior <ebjr.tux@gmail.com>
2021-06-06 05:24:28 -0300

    Validacao sintatica com Bean validation

--- src/main/java/com/edsonbjr/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/edsonbjr/cursomc/resources/CategoriaResource.java
@@ -4,6 +4,8 @@ import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
@@ -36,7 +38,8 @@ public class CategoriaResource {
 	}
 	
 	@PostMapping
-	public ResponseEntity<Void> insert(@RequestBody Categoria obj) {
+	public ResponseEntity<Void> insert(@Valid @RequestBody CategoriaDTO objDTO) {
+		Categoria obj = service.fromDTO(objDTO);
 		obj =  service.insert(obj);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
 				.path("/{id}").buildAndExpand(obj.getId()).toUri();
@@ -44,7 +47,8 @@ public class CategoriaResource {
 	}
 	
 	@PutMapping(value = "/{id}")
-	public ResponseEntity<Void> udpate(@RequestBody Categoria obj, @PathVariable Integer id) {
+	public ResponseEntity<Void> udpate(@Valid @RequestBody CategoriaDTO objDTO, @PathVariable Integer id) {
+		Categoria obj = service.fromDTO(objDTO);
 		obj.setId(id);
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
--- src/main/java/com/edsonbjr/cursomc/services/CategoriaService.java
+++ src/main/java/com/edsonbjr/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.edsonbjr.cursomc.domain.Categoria;
+import com.edsonbjr.cursomc.dto.CategoriaDTO;
 import com.edsonbjr.cursomc.repositories.CategoriaRepository;
 import com.edsonbjr.cursomc.services.exceptions.DataIntegrityException;
 import com.edsonbjr.cursomc.services.exceptions.ObjectNotFoundException;
@@ -55,5 +56,9 @@ public class CategoriaService {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction),
 				orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDTO) {
+		return new Categoria(objDTO.getId(), objDTO.getNome());
 	}
 }
